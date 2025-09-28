f97b5e342d72274fa3ce9925dab601a2bef29296
FelixManFongCheung <manfongcheung74@gmail.com>
2025-09-28 14:18:28 +0200

    added supabase in the mix

---
+++ src/app/middleware.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl;
+  if (pathname.startsWith("/api/")) {
+    return NextResponse.next();
+  }
+}
